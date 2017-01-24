a73fab829b57a3d3e9fc9c51983656891511041e
Kunal Varma <kunalvarma05@gmail.com>
2017-01-24 14:24:50 +0530

    Random background image

--- js/app.js
+++ js/app.js
@@ -2,7 +2,22 @@
 var toggleChannelFormBtn = document.getElementsByClassName('toggle-channel-form')[0];
 // Channel Form
 var channelForm = document.getElementsByClassName('channel-form')[0];
-// Toggle Channel Form through Toggle Button
-toggleChannelFormBtn.addEventListener('click', function() {
-    channelForm.classList.toggle('hidden');
-});
+
+if (channelForm) {
+    // Toggle Channel Form through Toggle Button
+    toggleChannelFormBtn.addEventListener('click', function() {
+        channelForm.classList.toggle('hidden');
+    });
+}
+// Auth Background
+var imgNum = Math.floor(Math.random() * 3) + 1  ;
+var authBg = document.getElementsByClassName('auth-page')[0];
+
+function randomBg() {
+    var img = "../images/bg" + imgNum + ".jpg";
+    authBg.style.backgroundImage = "url('" + img + "')";
+}
+
+if (authBg) {
+    randomBg();
+}
